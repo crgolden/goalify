53135f7a9e7b6015636ea14e822d6594d5196840
Chris Golden <crgolden@msn.com>
2015-10-19 16:25:59 -0600

    Redesigning navbar; removing sidebar

--- spec/features/visitors/navigation_spec.rb
+++ spec/features/visitors/navigation_spec.rb
@@ -1,7 +1,8 @@
 feature 'Navigation links', :devise do
   scenario 'view navigation links' do
     visit root_path
-    expect(page).to have_content 'Goalify'
+    expect(page).to have_content 'Goals'
+    expect(page).to have_content 'Users'
     expect(page).to have_content 'Sign in'
     expect(page).to have_content 'Register'
   end
